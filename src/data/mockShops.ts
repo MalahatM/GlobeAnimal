96dd6ac87d53827f72b2b5cdaf23ebd75d1c2328
your_username <malahat.m82@gmail.com>
2026-03-25 12:00:12 +0100

    mockdata added.3 butiks information.

---
+++ src/data/mockShops.ts
@@ -0,0 +1,26 @@
+export const mockShops = [
+  {
+    id: 1,
+    company_name: "Arken Zoo",
+    email: "kundtjanst@arkenzoo.se",
+    phone: " 0104906255",
+    address: "Gustaf Bergs väg 13, 183 34 Stockholm",
+    imageUrl: "https://static.bonniernews.se/ba/238e70ad-09b6-4b3d-879c-763831bf95ae.jpeg?crop=5352%2C3011%2Cx0%2Cy0"
+  },
+    {
+    id: 2,
+    company_name: "Vet Zoo",
+    email: "info@vetzoo.se",
+    phone: "010067880",
+    address: "Bergfotsgatan 1, Stockholm",
+    imageUrl: "https://images.ctfassets.net/we1x0sx4cl3b/5UEDNKYtAk8Fea6XNXNRKn/e8c5bf9a45aee85ff7fe78fa1d3eb381/Live_Shopping_6.jpg"
+  },
+    {
+    id: 3,
+    company_name: "Zooplus",
+    email: "info@zooplus.se",
+    phone: "020127636",
+    address: "Bergfotsgatan 1, Stockholm",
+    imageUrl: "https://www.faltoversten.se/app/uploads/2022/12/Faltoversten_liggande-Zoo-1-1-1440x810.webp"
+  }
+];
